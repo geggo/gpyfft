114c6721b505c9381c694dfe9fa213e5c1038bfa
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2012-05-25 20:10:27 +0200

    nearly done ;-)

--- gpyfft.pyx
+++ gpyfft.pyx
@@ -1,6 +1,11 @@
 # -*- coding: latin-1 -*-
-
+import cython
 import pyopencl as cl
+
+ctypedef long int voidptr_t
+
+DEF MAX_QUEUES = 5
+DEF MAX_WAITFOR_EVENTS = 10
 
 error_dict = {
     CLFFT_SUCCESS: 'no error',
@@ -19,7 +24,13 @@ class GpyFFT_Error(Exception):
         self.errorcode = errorcode
 
     def __str__(self):
-        return repr(error_dict.get(self.errorcode))
+        error_desc = error_dict.get(self.errorcode)
+        if error_desc is None:
+            try:
+                error_desc = cl.status_code.to_string(self.errorcode)
+            except ValueError:
+                error_desc = "unknown error %d", self.errorcode
+        return repr(error_desc)
 
 cdef inline bint errcheck(clAmdFftStatus result) except True:
     cdef bint is_error = (result != CLFFT_SUCCESS)
@@ -30,14 +41,12 @@ cdef inline bint errcheck(clAmdFftStatus result) except True:
 #main class
 #TODO: need to initialize (and destroy) at module level
 cdef class GpyFFT(object):
-    def __cinit__(self):
-        print "init clAmdFft"
+    def __cinit__(self): #TODO: add debug flag
         cdef clAmdFftSetupData setup_data
         errcheck(clAmdFftInitSetupData(&setup_data))
         errcheck(clAmdFftSetup(&setup_data))
 
     def __dealloc__(self):
-        print "closing clAmdFft"
         errcheck(clAmdFftTeardown())
 
     def get_version(self):
@@ -46,74 +55,161 @@ cdef class GpyFFT(object):
         return (major, minor, patch)
     
     def create_plan(self, context, tuple shape):
-        assert isinstance(context, cl.Context)
-        return Plan(context.obj_ptr, shape)
+        return Plan(context, shape, self)
      
         
+@cython.internal
 cdef class Plan(object):
 
     cdef clAmdFftPlanHandle plan
+    cdef object lib
 
     def __dealloc__(self):
         if self.plan:
-            print "destroy plan", hex(self.plan)
             errcheck(clAmdFftDestroyPlan(&self.plan))
-        pass
     
     def __cinit__(self):
         self.plan = 0
 
-    def __init__(self, context_handle, tuple shape):
-        cdef cl_context _context = <cl_context><long int>context_handle
-        cdef size_t lengths[3]
+    def __init__(self, context, tuple shape, lib):
+        self.lib = lib
+        if not isinstance(context, cl.Context):
+            raise TypeError('expected cl.Context as type of first argument')
+        
+        cdef cl_context context_handle = <cl_context><voidptr_t>context.obj_ptr
 
-        #TODO: errcheck shape
         ndim = len(shape)
-        _ndim = {1: CLFFT_1D, 2: CLFFT_2D, 3: CLFFT_3D}[ndim] #TODO: errcheck
+        if ndim not in (1,2,3):
+            raise ValueError('expected shape to be tuple of length 1,2 or 3')
+
+        cdef size_t lengths[3]
         for i in range(ndim):
             lengths[i] = shape[i]
         
-        for i in range(_ndim):
-            print lengths[i]
+        clAmdFftCreateDefaultPlan(&self.plan, context_handle, ndim, &lengths[0])
 
-        print "context:", hex(<long>_context)
-            
-        cdef clAmdFftPlanHandle plan
+    property precision:
+        def __get__(self):
+            cdef clAmdFftPrecision precision
+            errcheck(clAmdFftGetPlanPrecision(self.plan, &precision))
+            return precision
+        def __set__(self, clAmdFftPrecision value):
+            errcheck(clAmdFftSetPlanPrecision(self.plan, value))
 
-        clAmdFftCreateDefaultPlan(
-            &self.plan,
-             _context,
-             _ndim,
-             &lengths[0], 
-                                            )
-        print "plan:", hex(self.plan)
-        #TODO: set precision
-        #TODO: set strides
+    property scale_forward:
+        def __get__(self):
+            cdef cl_float scale
+            errcheck(clAmdFftGetPlanScale(self.plan, CLFFT_FORWARD, &scale))
+            return scale
+        def __set__(self, cl_float value):
+            errcheck(clAmdFftSetPlanScale(self.plan, CLFFT_FORWARD, value))
 
-    def get_precision(self):
-        cdef clAmdFftPrecision precision
-        errcheck(clAmdFftGetPlanPrecision(self.plan, &precision))
-        return precision
+    property scale_backward:
+        def __get__(self):
+            cdef cl_float scale
+            errcheck(clAmdFftGetPlanScale(self.plan, CLFFT_BACKWARD, &scale))
+            return scale
+        def __set__(self, cl_float value):
+            errcheck(clAmdFftSetPlanScale(self.plan, CLFFT_BACKWARD, value))
 
-    def bake(self, long int queue_handle):
-        
-        cdef cl_command_queue queue = <cl_command_queue>queue_handle
-        print hex(queue_handle), hex(<long>queue)
-                                              
+    property inplace:
+        def __get__(self):
+            cdef clAmdFftResultLocation placeness
+            errcheck(clAmdFftGetResultLocation(self.plan, &placeness))
+            return placeness == CLFFT_INPLACE
+        def __set__(self, value):
+            cdef clAmdFftResultLocation placeness
+            if value:
+                placeness = CLFFT_INPLACE
+            else:
+                placeness = CLFFT_OUTOFPLACE
+            errcheck(clAmdFftSetResultLocation(self.plan, placeness))
+
+    def bake(self, queue):
+        cdef cl_command_queue queue_handle = <cl_command_queue><voidptr_t>queue.obj_ptr
         errcheck(clAmdFftBakePlan(self.plan,
-                                  1, &queue,
+                                  1, &queue_handle,
                                   NULL, NULL))
 
-    def execute(self, 
-                queues, 
-                in_buffer, 
-                out_buffer = None,
-                forward = True, 
-                wait_for_events = None, 
-                temp_buffer = None,
-                ):
-        pass
+    def enqueue_transform(self, 
+                         queues, 
+                         in_buffers, 
+                         out_buffers = None,
+                         direction_forward = True, 
+                         wait_for_events = None, 
+                         temp_buffers = None,
+                         ):
+        cdef clAmdFftDirection direction
+        if direction_forward:
+            direction = CLFFT_FORWARD
+        else:
+            direction = CLFFT_FORWARD
+            
+        cdef cl_command_queue queues_[MAX_QUEUES]
+        if isinstance(queues, cl.CommandQueue):
+            queues = (queues,)
+        n_queues = len(queues)
+        for i, queue in enumerate(queues):
+            assert isinstance(queue, cl.CommandQueue)
+            queues_[i] = <cl_command_queue><voidptr_t>queue.obj_ptr
+            
+        cdef cl_event wait_for_events_array[MAX_WAITFOR_EVENTS]
+        cdef cl_event* wait_for_events_ = NULL
+        cdef n_waitfor_events = 0
+        if wait_for_events is not None:
+            n_waitfor_events = len(wait_for_events)
+            for i, event in enumerate(wait_for_events):
+                assert isinstance(event, cl.Event)
+                wait_for_events_array[i] = <cl_event><voidptr_t>event.obj_ptr
+            wait_for_events_ = &wait_for_events_array[0]
+
+        cdef cl_mem in_buffers_[2]
+        if isinstance(in_buffers, cl.Buffer):
+            in_buffers = (in_buffers,)
+        n_in_buffers = len(in_buffers)
+        assert n_in_buffers <= 2
+        for i, in_buffer in enumerate(in_buffers):
+            assert isinstance(in_buffer, cl.Buffer)
+            in_buffers_[i] = <cl_mem><voidptr_t>in_buffer.obj_ptr
+
+        cdef cl_mem out_buffers_array[2]
+        cdef cl_mem* out_buffers_ = NULL
+        if out_buffers is not None:
+            if isinstance(out_buffers, cl.Buffer):
+                out_buffers = (out_buffers,)
+            n_out_buffers = len(out_buffers)
+            assert n_out_buffers in (1,2)
+            for i, out_buffer in enumerate(out_buffers):
+                assert isinstance(out_buffer, cl.Buffer)
+                out_buffers_array[i] = <cl_mem><voidptr_t>out_buffer.obj_ptr
+            out_buffers_ = &out_buffers_array[0]
+
+        cdef cl_mem tmp_buffers_ = NULL #TODO: same as above
+
+        errcheck(clAmdFftEnqueueTransform(self.plan,
+                                          direction,
+                                          n_queues,
+                                          &queues_[0],
+                                          n_waitfor_events,
+                                          &wait_for_events_[0],
+                                          NULL,
+                                          &in_buffers_[0],
+                                          out_buffers_,
+                                          tmp_buffers_))
+
+        
+        
+            
+        
+        
+
+        
+        
                 
+
+
+
+
 
 #gpyfft = GpyFFT()
 
@@ -122,4 +218,3 @@ cdef class Plan(object):
     #cdef Plan instance = Plan.__new__(Ref)
     #instance.plan = None
     #return instance
-#    pass
--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,6 @@
 import pyopencl as cl
+import pyopencl.array as cla
+import numpy as np
 import gpyfft
 
 G = gpyfft.GpyFFT()
@@ -11,13 +13,43 @@ queue = cl.CommandQueue(context)
 print "context:", hex(context.obj_ptr)
 print "queue:", hex(queue.obj_ptr)
 
-plan = G.create_plan(context, (1024,))
+nd_data = np.array([[1,0,1,0], [0,2,0,2]], dtype = np.complex64)
+cl_data = cla.to_device(queue, nd_data.T)
+cl_data_transformed = cla.empty_like(cl_data)
 
-print plan.get_precision()
+print "cl_data:"
+print cl_data
 
-plan.bake(queue.obj_ptr)
+plan = G.create_plan(context, cl_data.shape)
+
+print 'plan.precision:', plan.precision
+
+plan.scale_forward = 1
+print 'plan.scale_forward:', plan.scale_forward
+plan.scale_forward = 1
+print 'plan.scale_forward:', plan.scale_forward
+
+print 'plan.inplace:', plan.inplace
+plan.inplace = False
+print 'plan.inplace:', plan.inplace
+
+plan.bake(queue)
+plan.enqueue_transform((queue,), 
+                       (cl_data.data,),
+                       (cl_data_transformed.data,)
+                       )
+queue.finish()
+
+print 'cl_data:'
+print cl_data
+print 'cl_data_transformed:'
+print cl_data_transformed
+
+print 'fft(nd_data):'
+print np.fft.fftn(nd_data).T
 
 del plan
+del G
 
 
 #raw_input()
