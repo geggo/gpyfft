3c9877d24894d37ac1fb4fb105c07894eec5f9c6
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2012-05-25 15:22:15 +0200

    select 32/64bit library depending on platform

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os.path, platform
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-import os.path
+
 
 AMDFFT = r'C:\Program Files (x86)\AMD\clAmdFft'
 AMDAPP = r'C:\Program Files (x86)\AMD APP'
+
+bits,foo = platform.architecture()
+if bits == '64bit':
+    AMDFFTLIBDIR = os.path.join(AMDFFT, 'lib64')
+else:
+    AMDFFTLIBDIR = os.path.join(AMDFFT, 'lib32')
 
 setup(
     name = 'Gpyfft',
@@ -22,7 +29,7 @@ setup(
                              extra_compile_args = [],
                              extra_link_args = [],
                              libraries = ['clAmdFft.Runtime'],
-                             library_dirs = [os.path.join(AMDFFT, 'lib64/import')]
+                             library_dirs = [os.path.join(AMDFFTLIBDIR, 'import')]
                              )
                    ],
     )
