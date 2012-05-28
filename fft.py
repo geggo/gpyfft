ffb3e99daea0f9a574a4629a11f58dd896b4482c
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2012-05-28 21:21:00 +0200

    high level interface

---
+++ fft.py
@@ -0,0 +1,11 @@
+class FFT(object):
+    def __init__(self, input_arrays, output_arrays, axes = (-1,), direction = 'forward', fast_math = True):
+        pass
+
+    def execute(self):
+        pass
+
+    def update_arrays(input_array, output_array)
+
+#complex transform: 2x input_arrays real or 1x input_array, same output
+#real to complex: (forward) out_array.shape[axes][-1] = in_array.shape[axes][-1]//2 + 1
