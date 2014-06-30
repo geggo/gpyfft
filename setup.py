d4f1e8fd9b4e0ce8d187d48a4c6a65d9a5b10a59
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2014-06-30 09:55:11 +0200

    setup.py: adapted for Windows with AMD APP SDK

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-import os.path, platform
+import os, os.path, platform
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
-CLFFT_DIR = r'/Users/gregor/Devel/clFFT'
+CLFFT_DIR = r'C:\Users\lab\Devel\clFFT'
+
+CL_INCL_DIRs = []
+if 'Windows' in platform.system():
+    CL_DIR = os.getenv('AMDAPPSDKROOT')
+    CL_INCL_DIRS = [os.path.join(CL_DIR, 'include')]
 
 import Cython.Compiler.Options
 Cython.Compiler.Options.generate_cleanup_code = 2
 
+#TODO: see https://github.com/matthew-brett/du-cy-numpy
+
 setup(
     name = 'Gpyfft',
-    version = '0.2',
+    version = '0.2.1',
     description = 'A Python wrapper for the OpenCL FFT library clFFT by AMD',
     url = r"https://github.com/geggo/gpyfft",
     maintainer='Gregor Thalhammer',
@@ -19,13 +26,14 @@ setup(
     packages = ['gpyfft'],
     ext_modules = [Extension("gpyfft.gpyfftlib",
                              [os.path.join('gpyfft', 'gpyfftlib.pyx')],
-                             include_dirs = [".",
+                             include_dirs = [#".",
                                              os.path.join(CLFFT_DIR,'src', 'include'),
-                                             ],
+                                             ] + CL_INCL_DIRS,
                              extra_compile_args = [],
                              extra_link_args = [],
                              libraries = ['clFFT'],
-                             library_dirs = [os.path.join(CLFFT_DIR, 'src', 'library')]
+                             library_dirs = [os.path.join(CLFFT_DIR, 'src', 'library'),
+                                             ],
                              )
                    ],
     )
