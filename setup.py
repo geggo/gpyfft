831478fcf4121b60abbce25f7c966c817349c0fb
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2014-06-30 10:44:47 +0200

    setup.py: install clFFT.dll on Windows

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import os, os.path, platform
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 CLFFT_DIR = r'C:\Users\lab\Devel\clFFT'
 
-CL_INCL_DIRs = []
+CL_INCL_DIRS = []
 if 'Windows' in platform.system():
     CL_DIR = os.getenv('AMDAPPSDKROOT')
     CL_INCL_DIRS = [os.path.join(CL_DIR, 'include')]
@@ -14,6 +14,33 @@ Cython.Compiler.Options.generate_cleanup_code = 2
 
 #TODO: see https://github.com/matthew-brett/du-cy-numpy
 
+extensions = [
+    Extension("gpyfft.gpyfftlib",
+              [os.path.join('gpyfft', 'gpyfftlib.pyx')],
+              include_dirs = [os.path.join(CLFFT_DIR,'src', 'include'),] + CL_INCL_DIRS,
+              extra_compile_args = [],
+              extra_link_args = [],
+              libraries = ['clFFT'],
+              library_dirs = [os.path.join(CLFFT_DIR, 'src', 'library'),],
+              )
+    ]
+
+def copy_clfftdll_to_package():
+    import shutil
+    shutil.copy(
+        os.path.join(CLFFT_DIR, 'src', 'staging', 'clFFT.dll'),
+        'gpyfft')
+    
+    shutil.copy(
+        os.path.join(CLFFT_DIR, 'src', 'staging', 'StatTimer.dll'),
+        'gpyfft')
+    print "copied clFFT.dll, StatTimer.dll"
+                 
+package_data = {}
+if 'Windows' in platform.system():
+    copy_clfftdll_to_package()
+    package_data.update({'gpyfft': ['clFFT.dll', 'StatTimer.dll']},)
+
 setup(
     name = 'Gpyfft',
     version = '0.2.1',
@@ -22,19 +49,8 @@ setup(
     maintainer='Gregor Thalhammer',
     maintainer_email = 'gregor.thalhammer@gmail.com',
     license = 'LGPL',
-    cmdclass = {'build_ext': build_ext},
     packages = ['gpyfft'],
-    ext_modules = [Extension("gpyfft.gpyfftlib",
-                             [os.path.join('gpyfft', 'gpyfftlib.pyx')],
-                             include_dirs = [#".",
-                                             os.path.join(CLFFT_DIR,'src', 'include'),
-                                             ] + CL_INCL_DIRS,
-                             extra_compile_args = [],
-                             extra_link_args = [],
-                             libraries = ['clFFT'],
-                             library_dirs = [os.path.join(CLFFT_DIR, 'src', 'library'),
-                                             ],
-                             )
-                   ],
+    ext_modules = cythonize(extensions),
+    package_data = package_data
     )
 
