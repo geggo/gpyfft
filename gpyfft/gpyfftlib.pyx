51715f36920e5d5167975166fb92559b6bf6f585
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2016-09-22 11:43:02 +0200

    attempt to fix pointer-to-int conversion on 64bit Windows: use long long int

--- gpyfft/gpyfftlib.pyx
+++ gpyfft/gpyfftlib.pyx
@@ -11,7 +11,10 @@ import cython
 import pyopencl as cl
 from libc.stdlib cimport malloc, free
 
-ctypedef long int voidptr_t
+#ctypedef long int voidptr_t
+ctypedef long long int voidptr_t
+#from libc.stdint cimport uintptr_t
+#ctypedef size_t voidptr_t
 
 DEF MAX_QUEUES = 5
 DEF MAX_WAITFOR_EVENTS = 10
@@ -595,7 +598,7 @@ cdef class Plan(object):
                                           tmp_buffer_))
         
         #return tuple((cl.Event.from_cl_event_as_int(<long>out_cl_events[i]) for i in range(n_queues)))
-        return tuple((cl.Event.from_int_ptr(<long>out_cl_events[i], retain=False) for i in range(n_queues)))
+        return tuple((cl.Event.from_int_ptr(<long long>out_cl_events[i], retain=False) for i in range(n_queues)))
             
         
             
